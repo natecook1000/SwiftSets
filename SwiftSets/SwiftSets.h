980289653404ebdda4ff81e80720d0d9064f6da5
Nate Cook <natecook@gmail.com>
2015-02-09 00:29:49 -0500

    Fixes for OS X framework

--- SwiftSets/SwiftSets.h
+++ SwiftSets/SwiftSets.h
@@ -6,14 +6,8 @@
 //  Copyright (c) 2015 Nate Cook. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-//! Project version number for SwiftSets.
 FOUNDATION_EXPORT double SwiftSetsVersionNumber;
-
-//! Project version string for SwiftSets.
 FOUNDATION_EXPORT const unsigned char SwiftSetsVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <SwiftSets/PublicHeader.h>
-
 
